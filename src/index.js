34f2bd7e105e26c0212ea5ab1312cc733cad8de9
nithin1110 <106112831+nithin1110@users.noreply.github.com>
2023-08-17 12:19:06 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+    <p>I Am Nithin_Connects</p>
+  </div>,
+  document.getElementById("root")
+);
